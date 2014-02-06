054acbba1748797d6c72dd377583074e00454e44
Jonathan Castillo <jcastillo@evidon.com>
2014-02-05 22:21:07 -0500

    cleaned up gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gainer::Application.config.secret_token = 'bd1aa80bee05eb0ea1c2923409a77332dd6b1daa03a3a5b1432873b5afaafa3a3a138301874377fb9316da8d517dd51f891e659ba1c1dfefe050033071ea9800'
