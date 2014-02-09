4d4b9a62bf62bb01ef830f020d2e8a85040f53aa
Jonathan Castillo <jcastillo@evidon.com>
2014-02-09 00:38:38 -0500

    ability to create ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,23 +1,37 @@
 class IngredientsController < ApplicationController
-  def index
-  end
+  
+  # def index
+  #   @ingredients = Ingredient.all
+  # end
   
   def new
     @ingredient = Ingredient.new
   end
 
   def create
+    @ingredient = Ingredient.create(params[:ingredient])
+    if @ingredient.save
+      redirect_to "show" 
+    else
+      flash[:error] = @ingredient.errors.empty? ? "Error" : @ingredient.errors.full_messages.to_sentence
+      render "new"
+    end
   end
   
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  # def show
+  #   @ingredient = Ingredient.find(params[:id])
+  # end
+  
+  # def edit
+  #   @ingredient = Ingredient.find(params[:id])
+  # end
+  
+  # def update
+  #   @ingredient = Ingredient.find(params[:id])
+  #   @ingredient.update_attributes(params[:ingredient])
+  # end
+  
+  # def destroy
+  # end
+  
 end
