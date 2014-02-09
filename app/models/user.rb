76d604a0fc77bf428b4597a467c2655f6114e3cb
Jonathan Castillo <jcastillo@evidon.com>
2014-02-08 21:21:59 -0500

    registration form and user validamodel tions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :age, :gender, :weight, :feet, :inches, :name
   # attr_accessible :title, :body
   
   has_many :meals
   has_many :statuses
+  
+  validates :age, presence: true,  numericality: { greater_than: 12, less_than_or_equal_to: 80}
+  validates :gender, presence: true
+  validates :weight, presence: true, numericality: { greater_than: 30, less_than_or_equal_to: 650}
+  validates :feet, presence: true, numericality: { greater_than_or_equal_to: 4, less_than_or_equal_to: 7}
+  validates :inches, presence: true, numericality: { less_than: 12 }
 end
