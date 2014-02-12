dc4e797623dcc1362aaa82303ef180a48d849cd1
Jonathan Castillo <jcastillo@evidon.com>
2014-02-11 21:56:22 -0500

    Added ingredient show view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,11 +8,15 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.create(params[:ingredient])
     if @ingredient.save
-      redirect_to ingredient_path(@ingredient) 
+      redirect_to ingredient_path(@ingredient), flash: {success: "Ingredient added."}
     else
       flash[:error] = @ingredient.errors.empty? ? "Error" : @ingredient.errors.full_messages.to_sentence
       render new_ingredient_path
     end
+  end
+  
+  def show
+    @ingredient = Ingredient.find(params[:id])
   end
   
 end
