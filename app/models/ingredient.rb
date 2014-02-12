acef41c88023636743ecbc11f928cf4761831e7e
Jonathan Castillo <jcastillo@evidon.com>
2014-02-11 22:43:26 -0500

    changed number_field to text_field ineld in new meal form

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -10,7 +10,7 @@ class Ingredient < ActiveRecord::Base
   validates :carbs, presence: true, numericality: true
   validates :price, numericality: true, :format => { :with => /^\d{1,4}(\.\d{0,2})?$/ }, allow_nil: true
   # serving_size metric is ounces
-  validates :serving_size, presence: true, numericality: true
+  validates :serving_size, presence: true, numericality: { greater_than: 0 }
   validates :servings, presence: true, numericality: true
 
 end
