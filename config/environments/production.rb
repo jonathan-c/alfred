7244d7193cbddf8cf8bb7b7190efb948c6989358
Jonathan Castillo <jcastillo@evidon.com>
2014-07-21 22:32:35 -0400

    precompile jquery-ui.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,8 @@ Gainer::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery-ui.js )
-  
+  config.assets.precompile += %w( jquery-ui.js jquery-ui.css)
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
